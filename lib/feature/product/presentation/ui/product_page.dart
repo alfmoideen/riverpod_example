8bd06af1b3dc6e31914ffa8d8e23defdec418622
Althaf Moideenkutty <althaf.moideenkutty@vensure.com>
2024-06-13 17:14:39 +0530

    edits

--- lib/feature/product/presentation/ui/product_page.dart
+++ lib/feature/product/presentation/ui/product_page.dart
@@ -22,10 +22,71 @@ class _ProductScreenState extends ConsumerState<ProductScreen> {
   Widget build(
     BuildContext context,
   ) {
+    final productLoadingStatus = ref.watch(
+      productProvider.select(
+        (value) => value.productStatus,
+      ),
+    );
+    final products = ref.watch(productProvider.select(
+      (value) => value.products,
+    ));
+
+    final errorMessage = ref.watch(
+      productProvider.select((value) => value.errorMessage),
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Products'),
       ),
+      body: productLoadingStatus == ProductStatus.loading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : productLoadingStatus == ProductStatus.success
+              ? ListView(
+                  shrinkWrap: true,
+                  children: [
+                    ...products.map(
+                      (item) => Container(
+                        padding: const EdgeInsets.all(10),
+                        child: Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.network(
+                                item.image,
+                                height: 100,
+                                width: 100,
+                              ),
+                              Text(
+                                item.title,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                item.description,
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              : productLoadingStatus == ProductStatus.failed
+                  ? Center(
+                      child: Text(
+                        errorMessage,
+                      ),
+                    )
+                  : const SizedBox(),
     );
   }
 }
